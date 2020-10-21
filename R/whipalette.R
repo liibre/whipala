18b2ec091afcc20302b69d3a37296340376bcedc
saramortara <saramortara@gmail.com>
2020-10-21 19:31:30 -0300

    adding a new color palette based on Huhsunqu https://commons.wikimedia.org/w/index.php?curid=988731

---
+++ R/whipalette.R
@@ -0,0 +1,12 @@
+#' Los colores de la whipala
+#'
+#' Colores baseados en figura de Huhsunqu, Public Domain, https://commons.wikimedia.org/w/index.php?curid=988731
+#'
+#' @export
+whipalette <- c("#ffffffff", #white
+                "#fcdd09ff", #yellow
+                "#eb7711ff", #orange
+                "#da121aff", #red
+                "#742c64ff", #purple
+                "#0f47afff", #blue
+                "#078930ff") # green
